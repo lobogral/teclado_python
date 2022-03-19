00ce152436b88231c6bf636d2bb87e9acddb198b
Camilo Sabogal <lobogral@gmail.com>
2022-03-18 23:09:27 -0500

    Update conteo_sobrecarga.py

--- conteo_sobrecarga.py
+++ conteo_sobrecarga.py
@@ -1,2 +1,3 @@
 print("Esta es una idea de crear cuánto es la sobrecarga por dedos y por manos")
 print("Asignado distribuciones qwerty, dvorak, y colemak")
+print("Lo mejor es que se haga un analisis por cada teclado")
